f12d1ada994f5bf64a3c88a024a8328292015877
Merik Nugroho <mericknugroho@gmail.com>
2025-12-17 14:52:26 +0700

    first commit

---
+++ internal/handler/ping_handler.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/entertrans/go-base-project.git/pkg/response"
+	"github.com/gin-gonic/gin"
+)
+
+type PingHandler interface {
+	Ping(c *gin.Context)
+}
+
+type pingHandler struct{}
+
+func NewPingHandler() PingHandler {
+	return &pingHandler{}
+}
+
+func (h *pingHandler) Ping(c *gin.Context) {
+	response.SendResponse(c, http.StatusOK, "pong", "Server is running")
+}
